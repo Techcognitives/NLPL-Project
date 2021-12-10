cebe181e6f645f0aa19324718ca1ce4da13e8eb3
abhijeet <abhijeetgotad99@gmail.com>
2021-12-10 12:49:14 +0530

    bank details activity done

--- app/src/main/java/com/nlpl/ui/ui/activities/BankDetailsActivity.java
+++ app/src/main/java/com/nlpl/ui/ui/activities/BankDetailsActivity.java
@@ -1,12 +1,16 @@
 package com.nlpl.ui.ui.activities;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
@@ -87,23 +91,62 @@ public class BankDetailsActivity extends AppCompatActivity {
         okButton = (Button) findViewById(R.id.bank_details_ok_button);
         okButton.setEnabled(false);
 
-        if (!bankName.getText().toString().isEmpty() && !accountNo.getText().toString().isEmpty() && !reAccount.getText().toString().isEmpty() && !ifscCode.getText().toString().isEmpty()){
-            okButton.setBackground(getResources().getDrawable(R.drawable.button_active));
-            okButton.setEnabled(true);
-        }
+        bankName.addTextChangedListener(bankDetailsWatcher);
+        accountNo.addTextChangedListener(bankDetailsWatcher);
+        reAccount.addTextChangedListener(bankDetailsWatcher);
+        ifscCode.addTextChangedListener(bankDetailsWatcher);
+
     }
 
     public void onClickBankDetailsOk(View view) {
-        Intent i8 = new Intent(BankDetailsActivity.this, ProfileAndRegistrationActivity.class);
-        i8.putExtra("mobile2", mobile);
-        i8.putExtra("name2", name);
-        i8.putExtra("isPersonal", isPersonalDetailsDone);
+        if (accountNo.getText().toString().equals(reAccount.getText().toString())) {
+            Intent i8 = new Intent(BankDetailsActivity.this, ProfileAndRegistrationActivity.class);
+            i8.putExtra("mobile2", mobile);
+            i8.putExtra("name2", name);
+            i8.putExtra("isPersonal", isPersonalDetailsDone);
             i8.putExtra("isBank", true);
             i8.putExtra("isTrucks", isAddTrucksDone);
-            i8.putExtra("isDriver",isAddDriversDone);
-        i8.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        startActivity(i8);
-        overridePendingTransition(0, 0);
-        BankDetailsActivity.this.finish();
+            i8.putExtra("isDriver", isAddDriversDone);
+            i8.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(i8);
+            overridePendingTransition(0, 0);
+            BankDetailsActivity.this.finish();
+        } else {
+            AlertDialog.Builder my_alert = new AlertDialog.Builder(BankDetailsActivity.this);
+            my_alert.setTitle("Account number does not match");
+            my_alert.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            });
+            my_alert.show();
+        }
     }
+
+    private TextWatcher bankDetailsWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            String bankName1 = bankName.getText().toString().trim();
+            String accNo1 = accountNo.getText().toString().trim();
+            String reAccNo1 = reAccount.getText().toString().trim();
+            String ifscCode1 = ifscCode.getText().toString().trim();
+
+            if (!bankName1.isEmpty() && !accNo1.isEmpty() && !reAccNo1.isEmpty() && !ifscCode1.isEmpty()) {
+
+                okButton.setBackground(getResources().getDrawable(R.drawable.button_active));
+                okButton.setEnabled(true);
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
 }
