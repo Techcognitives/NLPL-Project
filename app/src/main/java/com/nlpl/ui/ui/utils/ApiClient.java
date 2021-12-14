12066881b165857e0c69e92c4202d0b1525bbf45
abhijeet <abhijeetgotad99@gmail.com>
2021-12-14 10:45:25 +0530

    abhijeet

---
+++ app/src/main/java/com/nlpl/ui/ui/utils/ApiClient.java
@@ -0,0 +1,35 @@
+package com.nlpl.ui.ui.utils;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+
+    private static Retrofit retrofit;
+    private static String BASE_URL = "http://65.2.3.41:8080";
+
+    private static Retrofit getRetrofit() {
+
+
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor).build();
+
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
+                .build();
+
+        return retrofit;
+    }
+
+//    public static UserService getUserService() {
+//        UserService userService = getRetrofit().create(UserService.class);
+//        return userService;
+//    }
+}
