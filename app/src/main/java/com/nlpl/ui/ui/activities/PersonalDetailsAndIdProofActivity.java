2ba449807837101e5f9e079b412356211eadfddb
abhijeet <abhijeetgotad99@gmail.com>
2021-12-16 14:00:21 +0530

    abhijeet

--- app/src/main/java/com/nlpl/ui/ui/activities/PersonalDetailsAndIdProofActivity.java
+++ app/src/main/java/com/nlpl/ui/ui/activities/PersonalDetailsAndIdProofActivity.java
@@ -2,15 +2,106 @@ package com.nlpl.ui.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.nlpl.R;
 
 public class PersonalDetailsAndIdProofActivity extends AppCompatActivity {
 
+    View action_bar;
+    TextView actionBarTitle, language;
+    ImageView actionBarBackButton;
+    Dialog languageDialog;
+
+    Button personalAddressButton;
+    View personalView;
+
+    Button panAndAadharButton;
+    View panView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_details_and_id_proof);
+
+        action_bar = findViewById(R.id.personal_details_id_proof_action_bar);
+        actionBarTitle = (TextView) action_bar.findViewById(R.id.action_bar_title);
+        actionBarBackButton = (ImageView) action_bar.findViewById(R.id.action_bar_back_button);
+        language = (TextView) action_bar.findViewById(R.id.action_bar_language_selector);
+
+        language.setText(getString(R.string.english));
+        language.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                languageDialog = new Dialog(PersonalDetailsAndIdProofActivity.this);
+                languageDialog.setContentView(R.layout.dialog_language);
+                languageDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+                WindowManager.LayoutParams lp2 = new WindowManager.LayoutParams();
+                lp2.copyFrom(languageDialog.getWindow().getAttributes());
+                lp2.width = WindowManager.LayoutParams.MATCH_PARENT;
+                lp2.height = WindowManager.LayoutParams.WRAP_CONTENT;
+                lp2.gravity = Gravity.BOTTOM;
+
+                languageDialog.show();
+                languageDialog.getWindow().setAttributes(lp2);
+
+                TextView english = languageDialog.findViewById(R.id.english);
+                TextView marathi = languageDialog.findViewById(R.id.marathi);
+                TextView hindi = languageDialog.findViewById(R.id.hindi);
+
+                english.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        language.setText(getString(R.string.english));
+                    }
+                });
+
+                marathi.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        language.setText(getString(R.string.marathi));
+                    }
+                });
+
+                hindi.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        language.setText(getString(R.string.hindi));
+                    }
+                });
+
+            }
+        });
+
+        actionBarTitle.setText("Personal Details");
+        actionBarBackButton.setVisibility(View.GONE);
+
+        //------------------------------------------------------------------------------------------
+        personalAddressButton = (Button) findViewById(R.id.personal_details_id_proof_personal_address_button);
+        panAndAadharButton = (Button) findViewById(R.id.personal_details_id_proof_pan_aadhar);
+
+        //------------------------------------------------------------------------------------------
+    }
+
+    public void onClickPersonalOrAadhar(View view) {
+        switch (view.getId()) {
+            case R.id.personal_details_id_proof_personal_address_button:
+
+
+                break;
+
+            case R.id.personal_details_id_proof_pan_aadhar:
+                break;
+        }
     }
 }
