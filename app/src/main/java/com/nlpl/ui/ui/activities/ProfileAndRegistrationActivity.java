d07b492902daaf1ce77467e88651d6fce5a50e1a
abhijeet <abhijeetgotad99@gmail.com>
2022-01-01 12:27:22 +0530

    API Add truck splash screen and otp screen

--- app/src/main/java/com/nlpl/ui/ui/activities/ProfileAndRegistrationActivity.java
+++ app/src/main/java/com/nlpl/ui/ui/activities/ProfileAndRegistrationActivity.java
@@ -67,7 +67,6 @@ public class ProfileAndRegistrationActivity extends AppCompatActivity {
     String userId, userIdAPI, phone, mobileNoAPI;
     ArrayList<String> arrayUserId, arrayMobileNo,  arrayPinCode, arrayName, arrayRole, arrayCity, arrayAddress, arrayRegDone;
 
-
     Button personalDetails, bankDetails, addTrucks, addDrivers;
     String mobile, name, address, pinCode, city, role, emailIdAPI;
     TextView nameTitle, mobileText, emailIdTextView, officeAddressTextView;
