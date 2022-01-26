201dcd4469b6355ef7c4bfb813ec21b311208867
DELL <rasikasa67@gmail.com>
2022-01-26 10:35:53 +0530

    API Add truck splash screen and otp screen

--- app/src/main/java/com/nlpl/model/UpdateCustomerBudget.java
+++ app/src/main/java/com/nlpl/model/UpdateCustomerBudget.java
@@ -1,24 +1,16 @@
 package com.nlpl.model;
 
 public class UpdateCustomerBudget {
-    private String is_bid_accpted_by_sp;
+    private String budget;
 
-    public UpdateCustomerBudget(String is_bid_accpted_by_sp) {
-        this.is_bid_accpted_by_sp = is_bid_accpted_by_sp;
+    public UpdateCustomerBudget(String budget) {
+        this.budget = budget;
     }
 
     @Override
     public String toString() {
         return "UpdateCustomerBudget{" +
-                "is_bid_accpted_by_sp='" + is_bid_accpted_by_sp + '\'' +
+                "budget='" + budget + '\'' +
                 '}';
-    }
-
-    public String getIs_bid_accpted_by_sp() {
-        return is_bid_accpted_by_sp;
-    }
-
-    public void setIs_bid_accpted_by_sp(String is_bid_accpted_by_sp) {
-        this.is_bid_accpted_by_sp = is_bid_accpted_by_sp;
     }
 }
